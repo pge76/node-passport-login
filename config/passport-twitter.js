8af5608faf213b1afae27e47342483026a22be9e
Patrick Gebhardt <patrick.gebhardt@gmail.com>
2021-01-02 03:35:50 +0100

    little bit of cleanup and prettify of UI

--- config/passport-twitter.js
+++ config/passport-twitter.js
@@ -8,21 +8,22 @@ module.exports = (passport) => {
         callbackURL: "/auth/twitter/callback"
     },
         (accessToken, tokenSecret, profile, cb) => {
-            console.log("accessToken", accessToken)
-            console.log("tokenSecret", tokenSecret)
-            console.log("profile", profile)
+//            console.log("accessToken", accessToken)
+//            console.log("tokenSecret", tokenSecret)
+//            console.log("profile", profile)
+            
+            console.log(`retrieved twitter user ${profile.username} [${profile.displayName}]`)
 
             User.findOne({ twitterExternalId: profile.id })
                 .then(user => {
-                    console.log("user", user)
                     // if we have no user, we create it, otherwise we return the found user
                     if (!user) {
                         let user = new User({ twitterExternalId: profile.id, name: profile.username, isTwitter: true })
+                        console.log("user not found yet, creating new", user)
                         user.save()
-                        console.log("user after save", user)
-
                         return cb(null, user)
                     } else {
+                        console.log("user already in db, logging in")
                         return cb(null, user)
                     }
                 })
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const ensureAuthenticated = require('../config/auth').ensureAuthenticated
 
 router.get('/', (req, res) => {
     res.render('welcome');
 });
-router.get('/dashboard', (req, res) => {
-    
-    res.render('dashboard', {
-        user: req.user
-    });
-});
 
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+  res.render('dashboard', {
+    user: req.user
+  })
+);
 
 
 module.exports = router;
